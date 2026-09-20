54e22ee805c96ff9aaf5b26d950934e23bc77098
abdalrahman <abdalrahmanalmalllah@gmail.com>
2026-09-20 09:47:05 +0200

    Added Base Routing

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
+from  dotenv import load_dotenv
+load_dotenv(".env")
+from routes import base
 app = FastAPI()
-
-@app.get("/welcome")
-def welcome():
-    return{
-        "message": "HI"
-    }
+# Fixed the typo here: "include_router" instead of "inculde_router"
+app.include_router(base.base_router)
